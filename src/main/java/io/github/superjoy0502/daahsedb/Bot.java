ba3bbf3bee230129c6ac0f386b480fe0cc0e565f
SuperJoy <superjoy0502@gmail.com>
2021-10-29 13:54:00 -0600

    Report Memory Usage every second

--- src/main/java/io/github/superjoy0502/daahsedb/Bot.java
+++ src/main/java/io/github/superjoy0502/daahsedb/Bot.java
@@ -4,6 +4,10 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Bot {
 
     public static void main(String[] arguments) throws Exception {
@@ -18,6 +22,13 @@ public class Bot {
                 .addEventListeners(verification)
                 .build()
                 .awaitReady();
+
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        Runnable task = () -> {
+            System.out.println("Currently used memory: "
+                    + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576) + "MB");
+        };
+        executorService.scheduleWithFixedDelay(task, 0, 1, TimeUnit.SECONDS);
 
     }
 
