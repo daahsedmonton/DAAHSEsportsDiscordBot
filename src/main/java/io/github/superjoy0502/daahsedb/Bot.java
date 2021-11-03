b1f315fabdaac91ce3560c140d8d17ccb391849e
SuperJoy <superjoy0502@gmail.com>
2021-11-03 00:19:17 -0600

    Status message & rules

--- src/main/java/io/github/superjoy0502/daahsedb/Bot.java
+++ src/main/java/io/github/superjoy0502/daahsedb/Bot.java
@@ -2,11 +2,19 @@ package io.github.superjoy0502.daahsedb;
 
 import io.github.superjoy0502.daahsedb.verification.Verification;
 import io.github.superjoy0502.daahsedb.verification.VerificationListener;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Emoji;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.awt.*;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +23,8 @@ public class Bot {
 
     public static void main(String[] arguments) throws Exception {
 
-        boolean isCanary = false;
+        boolean isCanary = true;
+        long startTime = System.nanoTime();
 
         VerificationListener verificationListener = new VerificationListener(isCanary);
         Verification verification = verificationListener.verification;
@@ -29,18 +38,181 @@ public class Bot {
                 .build()
                 .awaitReady();
 
-        api.getPresence().setActivity(Activity.watching("Youtube (version 0.1.2)"));
+        api.getPresence().setActivity(Activity.watching("Youtube (version Canary)"));
 
-//        PartyMaker partyMaker = new PartyMaker(api, api.getGuildById(guildId));
+        EmbedBuilder esb = new EmbedBuilder();
+        esb.setTitle(":pushpin: DAAHS Esports Discord Server Rules v0.1.3");
+        esb.setColor(new Color(12, 60, 105));
+        esb.setDescription("• The following rules define the rules of DAAHS Esports Discord Server.\n" +
+                "• These rules must be followed strictly, or else penalties may apply.\n" +
+                "• Please change your nickname to your real name.");
 
+        api.getTextChannelById(905337770132774933L).sendMessage(
+                esb.build()
+        ).queue();
+
+        EmbedBuilder esb2 = new EmbedBuilder();
+        esb2.setTitle("Discord Official Guideline", "https://discord.com/guidelines");
+        esb2.setColor(new Color(12, 60, 105));
+        esb2.setDescription("• The official guideline of Discord has to be followed in this server.\n");
+
+        api.getTextChannelById(905337770132774933L).sendMessage(
+                esb2.build()
+        ).queue();
+
+        EmbedBuilder esb3 = new EmbedBuilder();
+        esb3.setTitle("Respect each others");
+        esb3.setColor(new Color(12, 60, 105));
+        esb3.setDescription("• In a community, every person should enjoy the space.\n" +
+                "• Discriminations towards others are prohibited.\n" +
+                "• Mind your manners.\n" +
+                "• Do not swear.\n" +
+                "• The maximum rating permitted in messages is **PG**.");
+
+        api.getTextChannelById(905337770132774933L).sendMessage(
+                esb3.build()
+        ).queue();
+
+        EmbedBuilder esb4 = new EmbedBuilder();
+        esb4.setTitle("This is an Esports server");
+        esb4.setColor(new Color(12, 60, 105));
+        esb4.setDescription("• Esports is not just about gaming, but also sportsmanship.\n" +
+                "• Just like playing a fair sports game, you should treat people fair and honorable.\n" +
+                "• Know what you should do and what you should not do.\n" +
+                "• Do not perform immature acts such as spamming, mentioning everybody, etc.");
+
+        api.getTextChannelById(905337770132774933L).sendMessage(
+                esb4.build()
+        ).queue();
+
+        EmbedBuilder esb5 = new EmbedBuilder();
+        esb5.setTitle("Lastly");
+        esb5.setColor(new Color(12, 60, 105));
+        esb5.setDescription("• Have fun.");
+
+        api.getTextChannelById(905337770132774933L).sendMessage(
+                esb5.build()
+        ).queue();
+
+
+//        api.getTextChannelById(905275922075234376L).sendMessage(".").queue();
+
+        PartyMaker partyMaker = new PartyMaker(api, api.getGuildById(guildId));
+        User bot = api.getSelfUser();
+
+        updateStatusOnline(isCanary, startTime, api, bot);
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+
+            @Override
+            public void run() {
+                updateStatusOffline(isCanary, startTime, api, bot);
+            }
+
+        });
+
+    }
+
+    private static void updateStatusOnline(boolean isCanary, long startTime, JDA api, User bot) {
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
         Runnable task = () -> {
 
-            System.out.println("Currently used memory: "
-                    + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576) + "MB");
+            api.getTextChannelById(905275922075234376L).retrieveMessageById(905290567846592562L).queue(
+                    message -> {
+
+                        long endTime   = System.nanoTime();
+                        long totalTime = endTime - startTime;
+                        int hours = Math.toIntExact(TimeUnit.NANOSECONDS.toHours(totalTime));
+                        int minutes = Math.toIntExact(TimeUnit.NANOSECONDS.toMinutes(totalTime) -
+                                TimeUnit.HOURS.toMinutes(TimeUnit.NANOSECONDS.toHours(totalTime)));
+
+                        EmbedBuilder eb = new EmbedBuilder();
+                        eb.setAuthor(bot.getName(), null, bot.getEffectiveAvatarUrl());
+                        eb.setTitle(":green_circle: Online");
+                        eb.setColor(!isCanary ? new Color(12, 60, 105) : new Color(252, 164, 28));
+                        eb.addField(
+                                "Uptime",
+                                String.format(
+                                        "%s hours %s minutes",
+                                        hours,
+                                        minutes
+                                ),
+                                true
+                        );
+                        eb.addField(
+                                "Used Memory",
+                                String.format(
+                                        "%s / %s",
+                                        ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576) + "MB",
+                                        (Runtime.getRuntime().maxMemory() / 1048576) + "MB"
+                                ),
+                                true
+                        );
+                        eb.setFooter(bot.getName());
+                        eb.setTimestamp(Instant.now());
+
+                        message.editMessage(
+                                " "
+                        ).queue();
+
+                        message.editMessage(
+                                eb.build()
+                        ).queue();
+
+                    }
+            );
 
         };
-        executorService.scheduleWithFixedDelay(task, 0, 1, TimeUnit.SECONDS);
+        executorService.scheduleWithFixedDelay(task, 0, 1, TimeUnit.MINUTES);
+    }
+
+    private static void updateStatusOffline(boolean isCanary, long startTime, JDA api, User bot) {
+
+        api.getTextChannelById(905275922075234376L).retrieveMessageById(905290567846592562L).queue(
+                message -> {
+
+                    long endTime   = System.nanoTime();
+                    long totalTime = endTime - startTime;
+                    int hours = Math.toIntExact(TimeUnit.NANOSECONDS.toHours(totalTime));
+                    int minutes = Math.toIntExact(TimeUnit.NANOSECONDS.toMinutes(totalTime) -
+                            TimeUnit.HOURS.toMinutes(TimeUnit.NANOSECONDS.toHours(totalTime)));
+
+                    EmbedBuilder eb = new EmbedBuilder();
+                    eb.setAuthor(bot.getName(), null, bot.getEffectiveAvatarUrl());
+                    eb.setTitle(":red_circle: Offline");
+                    eb.setColor(!isCanary ? new Color(12, 60, 105) : new Color(252, 164, 28));
+                    eb.addField(
+                            "Last Uptime",
+                            String.format(
+                                    "%s hours %s minutes",
+                                    hours,
+                                    minutes
+                            ),
+                            true
+                    );
+
+                    LocalDateTime myDateObj = LocalDateTime.now();
+                    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E., MMM. dd\nHH:mm:ss");
+                    String formattedDate = myDateObj.format(myFormatObj);
+
+                    eb.addField(
+                            "Offline Since",
+                            formattedDate.toString(),
+                            true
+                    );
+                    eb.setFooter(bot.getName());
+                    eb.setTimestamp(Instant.now());
+
+                    message.editMessage(
+                            " "
+                    ).queue();
+
+                    message.editMessage(
+                            eb.build()
+                    ).queue();
+
+                }
+        );
 
     }
 
